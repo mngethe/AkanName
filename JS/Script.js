96c4b2a7d276f910cbc63cd3a5de7d6990efc3e2
mngethe <njeringethe@gmail.com>
2022-09-30 10:14:59 +0300

    add project structure

---
+++ JS/Script.js
@@ -0,0 +1,3 @@
+const MALE_NAMES{"Kwasi" "Kwadwo" "Kwabena" "Kwaku" "Yaw" "Kofi" "Kwame"}
+const FEMALE_NAMES{"Akosua" "Adwoa" "Abenaa" "Akua" "Yaa" "Afua" "Ama"}
+const DAYS_OF_THE_WEEK{"sunday" "monday" "tuesday" "wednesday" "thursday" "friday" "saturday"}
