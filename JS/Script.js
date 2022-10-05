c3ebaa053394f10b3a85858a138a58ad225de5b5
mngethe <njeringethe@gmail.com>
2022-10-05 08:02:35 +0300

    add project progress

--- JS/Script.js
+++ JS/Script.js
@@ -1,15 +1,12 @@
-const MALE_NAMES = { 
-    ("Kwasi", 
-    "Kwadwo", 
-    "Kwabena", 
-    "Kwaku", 
-    "Yaw", 
-    "Kofi",
-    "Kwame") 
-}
+const MALE_NAMES = {"Kwasi":"Kwadwo","Kwabena":"Kwaku","Yaw":"Kofi","Kwame":};
+const FEMALE_NAMES = {"Akosua":"Adwoa","Abenaa":"Akua", "Yaa": "Afua", "Ama":};
+const DAYS_OF_THE_WEEK = {"sunday":"monday", "tuesday": "wednesday", "thursday": "friday", "saturday":};
 
-const FEMALE_NAMES = { ("Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama") }
-const DAYS_OF_THE_WEEK = { ("sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday") }
+    var day = "null"
+    var month = "null"
+    var year = "null"
+    var gender = "null"
+    var century ="null"
 
 get_user_input
     user_day_of_birth, 10
@@ -18,36 +15,39 @@ get_user_input
     user_gender, F
 
     //calculate day birth
-  //  var day = prompt("enter your date of birth")
-    //var month = prompt("enter your month of birth")
-    //var year = prompt("enter your year of birth")
-    //var century =year . slice(0,2)
+    //get user's akanname
 
-
-
-
-
-
-
-Day-of-the week (d) = ( ( (19/4) -2*19-1) + ((5*89/4) ) + ((26*(11+1)/10)) + 10 ) % 7
-
-
-
-function getAkhnaName {
-    if (gender === "M");
-if (gender.toLocateLowerCase ("m") .startsWith ) {
-    name = MALE_NAMES ( indexOfDay)
+    function get_user_input(){
+        var day = prompt("enter your date of birth")
+        var month = prompt("enter your month of birth")
+        var year = prompt("enter your year of birth")
+        var gender = prompt("enter your gender")
+        var century =year . slice(0,2)
+    }
     
-} else if (gender === "F"); {
-    if (gender.toLocateLowerCase ("f") .startsWith) name = FEMALE_NAMES (indexOfDay)
-} else !(gender "M"||"F"); { 
-    (console.log ("Invalid Gender"))
-}
-var userAkhanName = getAkhnaName (day, month, year, gender);
-console.log ("Your Name Is" + userAkhanName);
 
 
+function getakanName(10, 11, 1989,f){
+     var dayOfTheWeek = ( ( (19/4) -2*19-1) + ((5*89/4) ) + ((26*(11+1)/10)) + 10 ) % 7
+    //Sartuday
+
+      var indexOfDay =DAYS_OF_THE_WEEK.indexOf(dayOfTheWeek);
+      var name = null;
+
+      if (gender.toLocaleLowerCase().startsWith(f)){
+          name = FEMALE_NAMES [indexOfDay]
+       }else if (gender.toLocaleLowerCase(),startsWith('m')){
+          name= MALE_NAMES[indexOfDay]
+       }else{
+            console.log("invalid gender!")
+       }   
+
+       return name;
 }
+
+get_user_input()
+var userAkanName = getakanName(10, 11, 1989,f);
+console.log("your name is" + userAkanName);
 
 
 
--- README.md
+++ README.md
@@ -3,23 +3,32 @@ Project name:
 
 Project description:
   AKANNAME
+  Akan names are derived from Ghanian culture. Frequently in Ghana, children are given their first name as a 'day name' which corresponds to the day in the week they were born
+
 
 Author(s) information:
   Margaret Ngethe
   phone num:0720502929
 
 Setup instructions - includes any scripts that need to be run if necessary.
+  script.js
 BDD.
+  Title: Get Akan name
+
+   As a user.
+    I want to I input my date of birth.
+    so that I can get my Akan name
 
 
 Technologies used.
-   Javascript function,flows and logical operators
    HTML
-   Custom.css
+   CSS
+   Javascript
+   Git
 
 
 Contact information.
-  phone num:0720502929
+  MARGARET
 
 License and Copyright information.
   (C).2022 Njeri Ngethe
